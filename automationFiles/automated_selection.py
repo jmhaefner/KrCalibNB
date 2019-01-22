f752bd2ac44f2bb7a306a77f919206c1743e38e7
Jonathan Haefner <jmhaefn@gmail.com>
2019-01-22 12:09:39 -0500

    Add automated selection, correction, summary, and summary plots

---
+++ automationFiles/automated_selection.py
@@ -0,0 +1,99 @@
+import os
+import time
+from nbconvert.preprocessors import ExecutePreprocessor
+import nbformat
+import subprocess
+import sys
+# adapted from http://tritemio.github.io/smbits/2016/01/02/execute-notebooks/
+
+if len(sys.argv) == 2:
+    run_min = int(sys.argv[1])
+    run_max = run_min + 1
+elif len(sys.argv) == 3:
+    run_min = int(sys.argv[1])
+    run_max = int(sys.argv[2])
+else:
+    print('Run as \"python automated_summary.py <run_no>\" or \"python automated_summary.py <run_min> <run_max>\"')
+    exit()
+
+max_time = 1200 # max seconds for a single cell
+
+low_stat_runs = [ 6519, 6402, 6401, 6400, 6343 ] # fail at bin size 50 but do ok at smaller
+crit_low_runs = [ 6518, 6514, 6507, 6476, 6405, 6404, 6399, 6398, 6397, 6396, 6393, 6363, 6586 ] # too low to ever work
+bad_runs = [ 6519, 6518, 6514, 6507, 6476, 6405, 6404, 6402, 6401, 6400, 6399, 6398, 6397, 6396, 6393, 6363, 6343 ] # fail at bin size 50
+
+for run_number in range(run_min, run_max):
+
+    print()
+    run_directory = "/Volumes/NEXT_data/IC_Data/kdst/"+str(run_number)+"/"
+
+    #if not run_number in bad_runs:
+    #    print('SKIPPING', run_number)
+    #    continue
+
+    try:
+        print('Looking at data in', run_directory)
+        dirlist = os.listdir(run_directory)
+        last_file_number = -1
+        last_file = ""
+        for file_name in dirlist:
+            check_number = file_name
+            check_number = check_number[check_number.find('_')+1:]
+            check_number = check_number[:check_number.find('_')]
+            try:
+                if int(check_number) > last_file_number:
+                    last_file_number = int(check_number)
+                    last_file = check_number
+            except:
+                pass
+
+        print('Max file number =', last_file)
+
+        end_tags = dirlist[-1][-23:-3]
+
+        # Make a duplicate of the flexible notebook
+        print('Duplicating', run_number, '...')
+        flex_nb_location = "/Users/jmhaefner/Development/KryptonCalibration/KrCalibNB_JMH/KrCalibNB/automationFiles/kr_selection_and_filter_TPLT.ipynb"
+
+        flex_nb = open(flex_nb_location, "r")
+        mod_contents = flex_nb.read()
+        mod_contents = mod_contents.replace("<RUN_PARAMS>", "run = "+str(int(run_number)))
+        mod_contents = mod_contents.replace("<RUN_NUMBER>", str(int(run_number)))
+        mod_contents = mod_contents.replace("<LAST_FILE>", str(int(last_file)))
+        mod_contents = mod_contents.replace("<END_TAGS>", end_tags)
+        flex_nb.close()
+
+        # make the output folder for the summary
+        try:
+            summary_directory = "/Users/jmhaefner/Development/KryptonCalibration/KrCalibNB_JMH/KrCalibNB/doc/img/r"+str(run_number)+"/"
+            bash_mkdir = "mkdir " + summary_directory
+            print('[bash]:', bash_mkdir)
+            process = subprocess.Popen(bash_mkdir, stdout=subprocess.PIPE, shell=True).wait()
+        except:
+            print('New folder not made; already exists')
+
+        out_nb_prefix = "/Users/jmhaefner/Development/KryptonCalibration/KrCalibNB_JMH/KrCalibNB/krSelectionAndFilter/kr_selection_and_filter_"
+        out_nb_location = out_nb_prefix+str(run_number)+".ipynb"
+        out_nb = open(out_nb_location, "w")
+        out_nb.write(mod_contents)
+        out_nb.close()
+
+        print('New notebook:',out_nb_location)
+
+        t0 = time.time()
+        write_notebook = out_nb_location
+        print('Calculating notebook  ', out_nb_location)
+        print('Outputting to         ', write_notebook)
+        nb = nbformat.read(open(out_nb_location), as_version=4)
+        ep = ExecutePreprocessor(timeout=max_time, kernel_name='python3')
+        ep.preprocess(nb, {'metadata': {'.': './'}})
+        nbformat.write(nb, open(write_notebook, mode='wt'))
+        dt = time.time() - t0
+        print('Running time:', round(dt/60.0,1), 'm')
+
+    except (KeyboardInterrupt, SystemExit):
+        raise
+
+    except Exception as e:
+        print(e)
+        print('run', str(run_number), 'could not be analyzed')
---
+++ doc/automated_summary.py
@@ -0,0 +1,55 @@
+# Run as "python automated_summary.py <run_no>"
+#
+# Assumes the existance of krCalib_TPLT.tex, and slides/runTPLT.tex.
+# Creates version of those two files where TPLT is replaced by the
+# run n umber. krCalib_ABCD.tex should be typesetable by TeXShop.
+import sys
+
+if not len(sys.argv) == 2:
+    print('Run as \"python automated_summary.py <run_no>\"')
+    exit()
+
+run_number = int(sys.argv[1])
+
+# Read in the variables and their values
+vals_file_location = '/Users/jmhaefner/Development/KryptonCalibration/KrCalibNB_JMH/KrCalibNB/doc/data_'
+vals_file_location = vals_file_location+str(run_number)+'/vals_'+str(run_number)+'.txt'
+vals_file = open(vals_file_location, "r")
+
+vars = []
+vals = []
+
+line = vals_file.readline()
+while line:
+    line = line.replace('\n', '')
+    new_var = line[:line.find('=')]
+    new_val = line[line.find('=')+1:]
+    vars.append(new_var)
+    vals.append(new_val)
+    line = vals_file.readline()
+
+# Read in the template, replacing all of the dummy variables with their values
+template_location = '/Users/jmhaefner/Development/KryptonCalibration/KrCalibNB_JMH/KrCalibNB/doc/slides/runTPLT.tex'
+template = open(template_location, "r")
+mod_contents = template.read()
+for i in range(len(vars)):
+    name_to_replace = '<<'+vars[i]+'>>'
+    mod_contents = mod_contents.replace(name_to_replace, vals[i])
+template.close()
+
+# Save the new file
+output_location = '/Users/jmhaefner/Development/KryptonCalibration/KrCalibNB_JMH/KrCalibNB/doc/slides/run'+str(run_number)+'.tex'
+output = open(output_location, "w")
+output.write(mod_contents)
+output.close()
+
+# Duplicate the krCalib template with the correct run number
+wrap_template_location = '/Users/jmhaefner/Development/KryptonCalibration/KrCalibNB_JMH/KrCalibNB/doc/krCalib_TPLT.tex'
+wrap_template = open(wrap_template_location, "r")
+wrap_mod_contents = wrap_template.read()
+wrap_mod_contents = wrap_mod_contents.replace('<<RUNNUMBER>>', str(run_number))
+wrap_template.close()
+wrap_output_location = '/Users/jmhaefner/Development/KryptonCalibration/KrCalibNB_JMH/KrCalibNB/doc/krCalib_'+str(run_number)+'.tex'
+wrap_output = open(wrap_output_location, "w")
+wrap_output.write(wrap_mod_contents)
+wrap_output.close()
